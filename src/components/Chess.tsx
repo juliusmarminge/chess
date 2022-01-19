754fedfdf5a2b398b15a5340d6553dcf7b144333
Julius <julius0216@outlook.com>
2022-01-19 10:27:06 +0100

    Team is now taking turns making moves

--- src/components/Chess.tsx
+++ src/components/Chess.tsx
@@ -24,24 +24,21 @@ class Chess extends React.Component<MyProps, MyState>{
     constructor(props : MyProps) {
         super(props);
         this.state = {
-            board        : initBoard(),
+            board: initBoard(),
             selectedPiece: null,
             possibleMoves: [],
             turn: "white"
         }
     }
     selectPiece = (newX: number, newY: number) : void => {
-        let newBoard = this.state.board.slice();
-        let clickedPiece = newBoard[newY][newX];
+        let clickedPiece = this.state.board[newY][newX];
         if (findMoveInMoves(clickedPiece.position, this.state.possibleMoves)) {
-            let [x,y] = this.state.selectedPiece!;
-            let oldPiece = newBoard[y][x];
-            newBoard[newY][newX].typ = oldPiece.typ;
-            newBoard[newY][newX].team = oldPiece.team;
-            newBoard[y][x].typ = "free";
-            newBoard[y][x].team = "free";
-            // Send socket move has been done.
+            this.makeMove(this.state.selectedPiece!, clickedPiece.position);
+            return;
         }
+        if (clickedPiece.team !== this.state.turn)
+            return;
+        let newBoard = this.state.board.slice();
         let newSelectedPiece: Position | null = null;
         if (clickedPiece.typ !== "free") {
             if (clickedPiece.selected) {
@@ -59,14 +56,35 @@ class Chess extends React.Component<MyProps, MyState>{
         }
         let newMoves = newSelectedPiece ? getPossibleMoves(clickedPiece, newBoard) : [];
         this.setState({
-            board        : newBoard,
+            board: newBoard,
             selectedPiece: newSelectedPiece,
             possibleMoves: newMoves
         });
-    };
+    }
+
+    makeMove(oldPos: Position, newPos: Position) {
+        let newBoard = this.state.board.slice();
+        let oldPiece = newBoard[oldPos[1]][oldPos[0]];
+        let newPiece = newBoard[newPos[1]][newPos[0]];
+        newPiece.typ = oldPiece.typ;
+        newPiece.team = oldPiece.team;
+        oldPiece.typ = "free";
+        oldPiece.team = "free";
+        oldPiece.selected = false;
+        this.setState({
+            board: newBoard,
+            selectedPiece: null,
+            possibleMoves: [],
+            turn: this.state.turn === "white" ? "black" : "white"
+        });
+    }
+
     render(){
         return (
-            <Board boardPieces={this.state.board} selectPiece={this.selectPiece} moves={this.state.possibleMoves}/>
+            <>
+                <p className={"text-xl text-white font-bold p-2"}>Turn: {this.state.turn}</p>
+                <Board boardPieces={this.state.board} selectPiece={this.selectPiece} moves={this.state.possibleMoves}/>
+            </>
         );
     }
 }
